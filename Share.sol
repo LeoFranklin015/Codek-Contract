0eb21550f386eb75511848ab5dcb1370b6976bc3
Leo Franklin <94560875+LeoFranklin015@users.noreply.github.com>
2023-12-09 17:13:30 +0530

    Update Share.sol

--- Share.sol
+++ Share.sol
@@ -9,14 +9,18 @@ contract ShareNFT is ERC721, Ownable {
     uint256 public totalShares;
     address public Owner;
     mapping(address => uint256) public sharesOwned;
+    uint256 public contractBalance;
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalShares, uint256 _initialPrice) ERC721(_name, _symbol) Ownable(msg.sender) {
+    constructor(string memory _name, string memory _symbol, uint256 _totalShares, uint256 _initialPrice) ERC721(_name, _symbol) Ownable(msg.sender) payable {
         sharePrice = _initialPrice;
         // Mint initial shares to the contract owner
         _mint(msg.sender, _totalShares);
         Owner=msg.sender;
         sharesOwned[Owner] = _totalShares;
         totalShares=_totalShares;
+        if (msg.value > 0) {
+            payable(address(this)).transfer(msg.value);
+        }
     }
 
 function buyShares(uint256 _amount) external payable {
@@ -48,4 +52,13 @@ function buyShares(uint256 _amount) external payable {
     payable(recipient).transfer(proceeds);
 }
 
+    function sendEthToRecipient(address payable recipient, uint256 amount) external onlyOwner {
+        require(address(this).balance >= amount, "Insufficient contract balance");
+        require(recipient != address(0), "Invalid recipient address");
+        
+        recipient.transfer(amount);
+    }
+
 }
+
+
